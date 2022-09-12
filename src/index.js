8e65e8c2fa2a1a21190469fbed846edcecfc457b
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-09-12 15:05:38 +0500

    added redux persist

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from 'store'
+import store from 'store'
 import App from './App'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from 'store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	</React.StrictMode>
 )
